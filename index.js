4b65713b72082239caa214d6a4ce14ea725ca4a7
unknown <missoliveira99@gmail.com>
2022-05-16 21:45:32 +0100

    fix linters

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // get menu icon element
 const hamburger = document.querySelector('.ham');
-const nav_menu = document.querySelector('.navmenu')
+const navMenu = document.querySelector('.navmenu');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
-  nav_menu.classList.toggle('active');
-})
+  navMenu.classList.toggle('active');
+});
 
 // get the bars of the hamburger menu
 
@@ -15,18 +15,17 @@ const contactLink = document.querySelector('.contact-link');
 
 const close = () => {
   hamburger.classList.toggle('active');
-  nav_menu.classList.toggle('active');
+  navMenu.classList.toggle('active');
 };
 
 portfolio.addEventListener('click', () => {
   close();
-})
+});
 
 aboutUs.addEventListener('click', () => {
   close();
-})
+});
 
 contactLink.addEventListener('click', () => {
   close();
-})
-
+});
