0b9bf55ec02db440400e5bad21b54d3679cd47fc
Mirela <missoliveira99@gmail.com>
2022-05-18 00:40:00 -0300

    add the details.css file

--- index.js
+++ index.js
@@ -42,46 +42,46 @@ body.append(section);
         popup.className = 'pop';
         const main = document.createElement('div');
         main.className = 'main';
-        popup.innerHTML = `<section class="background_color" id="works">
-        <section class="container1">
-          <div class="work_card">
-            <div class="Pics">
-              <img class="img_desk" src="./images_desk/Snapshoot_Portfolio.jpg" alt="basketball-illustration" />
-              <img class="img_mobi" src="./images_mobile/Snapshoot-icon.jpg" alt="man with dark hair" />
-            </div>
-        
-            <div class="desk_side_info">
-              <div class="tonic">
+        popup.innerHTML = `
+        <section id="works">
+        <section class="modal_container1">
+         
+
+              <div class="modal-header">
                 <h2>Tonic</h2>
+                <button class="close-button">&times;</button>
               </div>
-              <br />
-        
               <ul class="first_list">
                 <li>CANOPY</li>
                 <li>Back End Dev</li>
                 <li>2015</li>
               </ul>
-        
+  
               <ul class="desk_list">
                 <li>CANOPY</li>
                 <li>Back End Dev</li>
                 <li>2015</li>
               </ul>
-        
+
+            <div class="Pics">
+              <img class="img_desk img_modal" src="./images_desk/Snapshoot_Portfolio.jpg" alt="basketball-illustration" />
+              <img class="img_mobi" src="./images_mobile/Snapshoot-icon.jpg" alt="man with dark hair" />
+            </div>
+  
               <div class="content_paragraph">
                 <p>
                   A daily selection of privately personalized reads; no accounts
                   or sign-ups required.
                 </p>
               </div>
-        
+  
               <div class="desk_paragraph">
                 <p>
                   A daily selection of privately personalized reads; no accounts
                   or sign-ups required.
                 </p>
               </div>
-        
+  
               <div class="tech_list1">
                 <ul class="list">
                   <li>
@@ -89,13 +89,13 @@ body.append(section);
                       <img src="./images_mobile/htmltag.jpg" alt="html" />
                     </a>
                   </li>
-        
+  
                   <li>
                     <a href="">
                       <img src="./images_mobile/csstag.jpg" alt="css" />
                     </a>
                   </li>
-        
+  
                   <li>
                     <a href="">
                       <img src="./images_mobile/jstag.jpg" alt="js" />
@@ -103,9 +103,14 @@ body.append(section);
                   </li>
                 </ul>
               </div>
-        
-              <button class="see_project">Close</button>
-              <div class="active" id="overlay"></div> `;
+  
+              <button class="see_project">See live</button>
+              <button class="see_project">See source</button>
+  
+            </div>
+          </div>
+          </div>
+        </section>`;
 
               main.appendChild(popup);
               body.appendChild(main);  
